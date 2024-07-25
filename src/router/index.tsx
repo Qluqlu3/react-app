b74fffe0f40f70a39946f912f6fef466fbc4678d
Qluqlu3 <plksmcgof@gmail.com>
2024-07-25 22:56:43 +0900

    fix not found

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,37 +1,44 @@
-import { Router, RootRoute, Route, Outlet, NotFoundRoute } from '@tanstack/react-router';
+import {
+  Router,
+  RootRoute,
+  Route,
+  Outlet,
+  createRoute,
+  createRootRoute,
+  createRouter,
+  NotFoundRoute,
+} from '@tanstack/react-router';
 
 import { App } from '../App';
 import { About, NotFound, Home } from '../pages';
 
-const rootRoute = new RootRoute({
+const rootRoute = createRootRoute({
   component: () => (
     <>
       <App />
       <Outlet />
     </>
   ),
+  notFoundComponent: () => {
+    <NotFound />;
+  },
 });
 
-const indexRoute = new Route({
+const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: '/',
   component: () => <Home />,
 });
 
-const aboutRoute = new Route({
+const aboutRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: '/about',
   component: () => <About />,
 });
 
-// const notFoundRoute = new NotFoundRoute({
-//   getParentRoute: () => rootRoute,
-//   component: () => <NotFound />,
-// });
-
 const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
 
-export const router = new Router({ routeTree /** notFoundRoute */ });
+export const router = createRouter({ routeTree });
 
 declare module '@tanstack/react-router' {
   interface Register {
