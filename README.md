7ca172477d006aaf6ff1b15faf402ba39bdfde5b
Qluqlu3 <plksmcgof@gmail.com>
2024-08-10 01:56:39 +0900

    fix README

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## start
 
 ```zsh
-yarn run dev
+pnpm dev
 ```
 
 <!-- This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/layout/Navigation/Navigation.tsx
+++ src/layout/Navigation/Navigation.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 
-import { createBrowserHistory } from '@tanstack/history'
 import { Link } from '@tanstack/react-router'
-import clsx from 'clsx'
 
 export const Navigation: React.FC = () => {
-  const { location } = createBrowserHistory()
-  const currentPath = location.pathname
-
   const URL = {
     root: '/',
     about: '/about',
@@ -16,30 +11,36 @@ export const Navigation: React.FC = () => {
     csv: '/csv',
   } as const
 
+  const activeProps = {
+    style: {
+      color: '#22c55e',
+    },
+  }
+
   return (
     <div className='mb-5 mt-3 flex flex-row rounded-md bg-gray-700 p-2 text-3xl [&>div]:px-3 [&>div]:py-2  [&>div]:text-3xl hover:[&>div]:text-green-200'>
       <div>
-        <Link to={URL.root} className={clsx(currentPath === URL.root && 'text-green-500')}>
+        <Link to={URL.root} activeOptions={{ exact: true }} activeProps={activeProps}>
           Home
         </Link>
       </div>
       <div>
-        <Link to={URL.about} className={clsx(currentPath === URL.about && 'text-green-500')}>
+        <Link to={URL.about} activeProps={activeProps}>
           About
         </Link>
       </div>
       <div>
-        <Link to={URL.slate} className={clsx(currentPath === URL.slate && 'text-green-500')}>
+        <Link to={URL.slate} activeProps={activeProps}>
           Slate
         </Link>
       </div>
       <div>
-        <Link to={URL.chakra} className={clsx(currentPath === URL.chakra && 'text-green-500')}>
+        <Link to={URL.chakra} activeProps={activeProps}>
           Chakra
         </Link>
       </div>
       <div>
-        <Link to={URL.csv} className={clsx(currentPath === URL.csv && 'text-green-500')}>
+        <Link to={URL.csv} activeProps={activeProps}>
           CSV
         </Link>
       </div>
