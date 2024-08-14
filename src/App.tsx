1f1d57ef02e85a7fd277e82380991da4b662ebfd
Qluqlu3 <plksmcgof@gmail.com>
2024-08-15 00:33:08 +0900

    fix App and css

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,25 @@ import React from 'react'
 import { Link } from '@tanstack/react-router'
 
 import reactLogo from './assets/react.svg'
-import { Navigation } from './layout/Navigation'
 
 import viteLogo from '/vite.svg'
 
 const ICON_SIZE = 80
 
-export const App = () => {
+export const App: React.FC = () => {
   return (
-    <div>
+    <div className='w-full'>
       <div className='m-1 flex w-full items-center justify-center'>
-        <Link href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} width={ICON_SIZE} alt='Vite logo' />
-        </Link>
         <Link href='https://react.dev' target='_blank'>
           <img src={reactLogo} width={ICON_SIZE} alt='React logo' />
         </Link>
-        <h1 className='mx-2 text-7xl'>Vite + React</h1>
+        <h1 className='px-2 pb-5 pt-2 text-8xl font-bold'>React</h1>
+        <h1 className='text-6xl'> + </h1>
+        <Link href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} width={ICON_SIZE} alt='Vite logo' />
+        </Link>
+        <h1 className='px-2 pb-5 pt-2 text-8xl font-bold'>Vite</h1>
       </div>
-      <Navigation />
     </div>
   )
 }
